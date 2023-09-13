97b38786484d7513b880becb22886df6a4f9cf4a
Valera <kozevnikovvalera@gmail.ru>
2023-09-13 21:30:17 +0300

    finish all tasks

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  // eslint-disable-next-line no-use-before-define
+  console.log(a);
+  const a = 10;
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const counters = {};
+  for (const item of arr) {
+    const type = typeof item;
+    let counter = counters[type] || 0;
+    counters[type] = ++counter;
+  }
+  return counters;
+};
 
 module.exports = { countTypesInArray };
